ec13d3f00f94a735d60a57c5f6a6bb820fb7e432
Xavi Rodríguez-Rubi <82876792+xavirubi@users.noreply.github.com>
2021-12-07 15:54:13 +0100

    Update script.py

--- script.py
+++ script.py
@@ -24,9 +24,5 @@ def dayOfWeek():
             return res
 
 
-if __name__ == "__main__":
-    app.run(debug=True)
-
-
-from flask import Flask, request
-app = Flask(__name__)
+#if __name__ == "__main__":
+#    app.run(debug=True)
